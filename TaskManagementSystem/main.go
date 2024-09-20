869047d89db3b79bed4bca6bbf380486cb738553
Cahandarov <cahandarov@gmail.com>
2024-09-20 08:51:53 +0400

    first task

---
+++ TaskManagementSystem/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// First print
+	fmt.Println("Welcome to the Task Management System!")
+
+	// Second print
+	fmt.Println("Project start date is:", time.Date(2024, time.September, 18, 0, 0, 0, 0, time.UTC))
+
+	// Third print
+	fmt.Println("Project: Task Management System")
+}
