f14581a1d3080ca62a243c6597c9b1dc8749d49b
Cahandarov <cahandarov@gmail.com>
2024-09-28 00:23:05 +0400

    requested changes done

--- TaskManagementSystem/main.go
+++ TaskManagementSystem/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"strings"
 	"time"
+)
+
+const (
+	Low = iota + 1
+	Medium
+	High
 )
 
 func TaskUpdate(CompletedTasks int) {
@@ -14,39 +19,33 @@ func TaskUpdate(CompletedTasks int) {
 
 	RemainingTasks := NumberOfTotalTasks - CompletedTasks
 	if CompletedTasks != NumberOfTotalTasks {
-		ProjectStatus = "in progress"
+		ProjectStatus = "IN PROGRESS"
 	} else {
-		ProjectStatus = "finished"
+		ProjectStatus = "FINISHED"
 	}
 
 	var ProjectCompletionStatus bool
-	if ProjectStatus == "in progress" {
+	if ProjectStatus == "IN PROGRESS" {
 		ProjectCompletionStatus = false
 	} else {
 		ProjectCompletionStatus = true
 	}
 
-	const (
-		Low = iota + 1
-		Medium
-		High
-	)
-
 	var ProjectPhase string
 	switch {
 	case CompletedTasks < 30:
-		ProjectPhase = "Starting phase"
-	case 30 <= CompletedTasks && CompletedTasks <= 60:
-		ProjectPhase = "Midway"
-	case 60 <= CompletedTasks:
-		ProjectPhase = "Final phase"
+		ProjectPhase = "starting phase"
+	case CompletedTasks >= 30 && CompletedTasks <= 60:
+		ProjectPhase = "midway"
+	case CompletedTasks > 60:
+		ProjectPhase = "final phase"
 	default:
 		fmt.Println("Wrong entered data")
 	}
 
 	fmt.Printf("Tasks remaining %d out of %d\n", RemainingTasks, NumberOfTotalTasks)
-	fmt.Printf("Current project status: %s\n", strings.ToUpper(ProjectStatus))
-	fmt.Printf("Project is in the %s.\n", strings.ToLower(ProjectPhase))
+	fmt.Printf("Current project status: %s\n", ProjectStatus)
+	fmt.Printf("Project is in the %s.\n", ProjectPhase)
 	fmt.Printf("Task priorities: %d-Low, %d-Medium, %d-High\n", Low, Medium, High)
 	if CompletedTasks > 0 {
 		fmt.Println("Task list:")
@@ -67,9 +66,6 @@ func main() {
 	fmt.Println("Project start date is:", time.Date(2024, time.September, 18, 0, 0, 0, 0, time.UTC))
 
 	// Third print
-	fmt.Println("Project: Task Management System")
-	fmt.Println()
+	fmt.Println("Project: Task Management System\n")
 	TaskUpdate(95)
-	fmt.Println()
-	fmt.Println()
 }
